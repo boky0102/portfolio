2e5152fd20ff4b09e1c6549048fe57b5469911f8
boky0102 <bivankovi@protonmail.com>
2021-06-28 17:26:15 +0200

    Finished snapping of sections added some JS logic to site

--- mainscript.js
+++ mainscript.js
@@ -5,7 +5,25 @@ const container = document.getElementsByClassName('content')[0];
 
 const background = document.getElementById("landing-section");
 
+const circle  = document.getElementsByClassName("circ")[0];
+
+const work = document.getElementById("work-section")
+
+const workBtn = document.getElementById("work-button")
+
+const separator = document.getElementsByClassName("separator")[0];
+
+workBtn.onclick = () => {
+    console.log("Clicked")
+    work.scrollIntoView(({behavior: "smooth", block: "end"}))
+}
+
+
 
 container.onscroll = () => {
-    background.background.style.backgroundColor = "#EABA6B"
+    var scrollPos = container.pageYOffset;
+    console.log(scrollPos);
+    background.style.backgroundColor = "#EAD2AC"
+    circle.style.backgroundColor = "#EABA6B"
+    separator.style.backgroundColor = "#EABA6B"
 }
